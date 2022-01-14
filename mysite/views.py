7dd95be81710e60dd1bbccc2b49c26ea4e96133e
Vitaly <ivanov.arduan@yandex.ru>
2022-01-14 15:12:18 +0300

    Представление через TemplateView. Это еще проще.

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 
 
 def index(request):
@@ -12,6 +13,17 @@ def index(request):
     return render(request, 'mysite/index.html', context)
 
 
-class About(View):
-    def get(self, request):
-        return render(request, 'mysite/about.html', {})
+# class About(View):
+#     def get(self, request):
+#         return render(request, 'mysite/about.html', {})
+
+
+class About(TemplateView):
+    template_name = 'mysite/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(About, self).get_context_data(**kwargs)
+        context['title'] = 'О сайте'
+        context['name'] = 'Бесплатные объявления'
+        context['description'] = 'Lorem lkj jlkjd kjf; eiuiufhasjkldk'
+        return context
