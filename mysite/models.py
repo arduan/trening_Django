1641d48e0fe784fec83be2904f9d8ade81e57603
Vitaly <ivanov.arduan@yandex.ru>
2022-01-16 07:49:23 +0300

    Создал модель, миграции, суперпользователя и зарегистрировал модель в admin.py приложения.

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Patients(models.Model):
+    """
+    Первоначальная модель где будет фамилия, возраст и пульс
+    для тренировки вычисляемых полей.
+    """
+    name = models.CharField(max_length=30, verbose_name='Фамилия пациенте')
+    age = models.IntegerField(null=True, verbose_name='Возраст')
+    pulse = models.IntegerField(null=True, verbose_name='Пульс')
