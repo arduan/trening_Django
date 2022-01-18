e87ec1926e1addde73f45dbd187f0c9f2d42d69a
Vitaly <ivanov.arduan@yandex.ru>
2022-01-18 08:15:06 +0300

    Немного упростил импорты.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from mysite import views
-
+from .views import PatientsDetailView, PatientsListView
 
 urlpatterns = [
 
     path('', views.Index.as_view(), name='index'),
     path('about/', views.About.as_view(), name='about'),
-    path('patients_list_view/', views.PatientsListView.as_view(), name='patients'),
-    path('patients_list_view/<int:pk>/', views.PatientsDetailView.as_view(), name='patients_detail'),
+    path('patients_list_view/', PatientsListView.as_view(), name='patients'),
+    path('patients_list_view/<int:pk>/', PatientsDetailView.as_view(), name='patients-detail'),
 
     ]
