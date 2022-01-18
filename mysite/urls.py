17725fd010d5ca26d61ef1160e94a57c539ea1c5
Vitaly <ivanov.arduan@yandex.ru>
2022-01-18 08:33:10 +0300

    Немного упростил.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from mysite import views
-from .views import PatientsDetailView, PatientsListView
+from .views import PatientsDetailView, PatientsListView, About, Index
 
 urlpatterns = [
 
-    path('', views.Index.as_view(), name='index'),
-    path('about/', views.About.as_view(), name='about'),
-    path('patients_list_view/', PatientsListView.as_view(), name='patients'),
-    path('patients_list_view/<int:pk>/', PatientsDetailView.as_view(), name='patients-detail'),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+    path('patients_list/', PatientsListView.as_view(), name='patients'),
+    path('patients_list/<int:pk>/', PatientsDetailView.as_view(), name='patients-detail'),
 
     ]
