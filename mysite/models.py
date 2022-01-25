9ce030cab41baa6c90ed53434f5464953df6f4be
Vitaly <ivanov.arduan@yandex.ru>
2022-01-25 13:17:40 +0300

    Добавил новое поле в модель.

--- mysite/models.py
+++ mysite/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
 
-
 class Patients(models.Model):
     """
     Первоначальная модель где будет фамилия, возраст и пульс
     для тренировки вычисляемых полей.
     """
-    name = models.CharField(max_length=30, verbose_name='Фамилия пациенте')
-    age = models.IntegerField(null=True, verbose_name='Возраст')
-    pulse = models.IntegerField(null=True, verbose_name='Пульс')
-
+    name = models.CharField(max_length=30, null=True, blank=True, verbose_name='Фамилия пациенте')
+    age = models.IntegerField(null=True, blank=True, verbose_name='Возраст')
+    pulse = models.IntegerField(null=True, blank=True, verbose_name='Пульс')
+    date = models.DateTimeField (null=True, blank=True, verbose_name='Дата')
 
     def __str__(self):
         return self.name
