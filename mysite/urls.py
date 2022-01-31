e3b12316053f3d82ffb4290b36eb76a3d6ec9c0a
Vitaly <ivanov.arduan@yandex.ru>
2022-01-31 08:43:24 +0300

    Все формы работают через одни и тот же контролер.

--- mysite/urls.py
+++ mysite/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('patients_list/', PatientsListView.as_view(), name='patients'),
     path('patients_detail/<int:pk>/', PatientsDetailView.as_view(), name='detail'),
     path('patients_max/', PatientStatisticsView.as_view(), name='max'),
-    path('form_view/', views.my_form, name='form'),
-    path('index_form/', index_form, name='form2'),
-    path('index_form3/', index_form3, name='form3'),
+    path('form_view/',  MyCreateView.as_view(), name='form'),
+    path('index_form/',  MyCreateView.as_view(), name='form2'),
+    path('index_form3/', MyCreateView.as_view(), name='form3'),
     path('create/', MyCreateView.as_view(), name='create'),
 
 
