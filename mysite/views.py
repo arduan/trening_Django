69ec47d8c20f9ced6607ad59a5c166ccabeff2df
Vitaly <ivanov.arduan@yandex.ru>
2022-02-05 14:41:54 +0300

    Контролер удаления записи.

--- mysite/views.py
+++ mysite/views.py
@@ -76,7 +76,7 @@ class PacientUpdate(UpdateView):
 class PacientDeleteView(DeleteView):
     model = Patients
     success_url = '/patients_list/'
-
+    template_name = 'delete.html'
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['name'] = Patients.objects.all()
